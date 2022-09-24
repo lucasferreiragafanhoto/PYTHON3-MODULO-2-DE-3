4a0fbde4ef79b7de5d53566258c1a5bebf772006
Lucas Ferreira Silva <silvaflucas88@gmail.com>
2022-09-23 23:38:44 -0300

    Create ex038.py

---
+++ pythonexercicios/pycharmprojeto/pythonexercicios/ex038.py
@@ -0,0 +1,8 @@
+n1 = int(input('primeiro número'))
+n2 = int(input('segundo número'))
+if n1 > n2:
+ print('O PRIMEIRO valor é MAIOR')
+elif n2 > n1:
+ print('O SEGUNDO valor é maior')
+else:
+    print('os dois valores são iguais')
